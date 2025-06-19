697f479203205ce6768330fbb617026715f866b5
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-06-19 21:41:58 +0600

    added products count

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.off1efx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,8 +37,14 @@ async function run() {
 
     app.post("/productsIds",async(req,res)=>{
       const ids =req.body;
-      console.log(ids)
-      res.send([])
+      const idsWithObjectId =ids.map(id=> new ObjectId(id))
+      const query={
+        _id:{
+          $in:idsWithObjectId
+        }
+      }
+      const result =await productCollection.find(query).toArray();
+      res.send(result)
     })
 
     app.get("/productsCount", async (req, res) => {
